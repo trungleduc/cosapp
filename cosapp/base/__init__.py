e38a9cbcb30e0b5c02c0dc19292a7becfa19cedb
Etienne Lac <etienne.lac@safrangroup.com>
2021-11-13 10:02:54 +0100

    Add custom exceptions to `cosapp.base`

--- cosapp/base/__init__.py
+++ cosapp/base/__init__.py
@@ -6,3 +6,7 @@ from cosapp.systems import System
 from cosapp.drivers import Driver
 from cosapp.core.connectors import BaseConnector
 from cosapp.utils.surrogate_models import SurrogateModel
+
+# Custom exceptions
+from cosapp.ports import ScopeError, UnitError
+from cosapp.core.connectors import ConnectorError
