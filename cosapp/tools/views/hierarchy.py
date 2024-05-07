7db7d1162a64afb38d1d88252bc5a4b01f32a399
Etienne Lac <etienne.lac@safrangroup.com>
2024-05-07 18:08:23 +0200

    New function `tools.views.hierarchy`

---
+++ cosapp/tools/views/hierarchy.py
@@ -0,0 +1,78 @@
+from cosapp.base import System
+from math import inf
+from typing import List
+
+
+def hierarchy_content(system: System, depth=None, show_class=False) -> List[str]:
+    """Create content displayed by `hierarchy` as a list of strings.
+    """
+    if depth is None:
+        depth = inf
+    else:
+        depth = max(0, depth)
+    space = padding = " "
+    tab = 4 * space
+    
+    def get_content(s: System, level: int, hook=""):
+        if level <= depth:
+            indent = (level - 1) * tab if level > 0 else ""
+            info = f"{indent}{hook}{s.name}"
+            if show_class:
+                # padding = max(1, 10 - len(s.name)) * space
+                info += f"{padding}[{type(s).__name__}]"
+            content = [info]
+            if s.children and level == depth:
+                pass
+            else:
+                last = len(s.children) - 1
+                for i, child in enumerate(s.children.values()):
+                    if i == last or (child.children and level + 1 < depth):
+                        hook = "└── "
+                    else:
+                        hook = "├── "
+                    content.extend(get_content(child, level + 1, hook))
+            return content
+        
+        else:
+            return []
+    
+    return get_content(system, 0)
+
+
+def hierarchy(system: System, depth=None, show_class=False) -> None:
+    """Print a hierarchical representation of a system tree.
+
+    Parameters:
+    -----------
+    - system [System]: system of interest
+    - depth [int, optional]: max depth of the tree representation
+    - show_class [bool, optional]: if `True`, the system type is
+        shown for every sub-system in the tree. Defaults to `False`.
+    
+    Example:
+    --------
+    >>> head = Head('head')
+    >>> hierarchy(head)
+    head
+    └── child1
+        └── child11
+            ├── child111
+            └── :
+        ├── child12
+        └── child13
+    └── child2
+        ├── child21
+        └── :
+
+    >>> hierarchy(head, depth=2, show_class=True)
+    head [Head]
+    └── child1 [Child1]
+        ├── child11 [..]
+        ├── child12 [..]
+        └── child13 [..]
+    └── child2 [..]
+        ├── child21 [..]
+        └── :
+    """
+    content = hierarchy_content(system, depth, show_class)
+    print("\n".join(content))
---
+++ cosapp/tools/views/tests/test_hierarchy.py
@@ -0,0 +1,109 @@
+import pytest
+
+from cosapp.base import System
+from cosapp.tools.views.hierarchy import hierarchy_content
+from typing import List
+
+
+@pytest.fixture
+def composite():
+    """Generates test system tree:
+
+                 a
+         ________|________
+        |        |        |
+       aa       ab        ac
+      __|__           ____|____
+     |     |         |    |    |
+    aaa   aab       aca  acb  acc
+    """
+    def add_children(system: System, names: List[str]):
+        prefix = system.name
+        for name in names:
+            system.add_child(System(f"{prefix}{name}"))
+
+    a = System('a')
+    add_children(a, list('abc'))
+    add_children(a.aa, list('ab'))
+    add_children(a.ac, list('abc'))
+
+    return a
+
+
+@pytest.mark.parametrize("options, expected", [
+    (
+        dict(), [
+            'a',
+            '└── aa',
+            '    ├── aaa',
+            '    └── aab',
+            '├── ab',
+            '└── ac',
+            '    ├── aca',
+            '    ├── acb',
+            '    └── acc',
+        ],
+    ),
+    (
+        dict(show_class=True), [
+            'a [System]',
+            '└── aa [System]',
+            '    ├── aaa [System]',
+            '    └── aab [System]',
+            '├── ab [System]',
+            '└── ac [System]',
+            '    ├── aca [System]',
+            '    ├── acb [System]',
+            '    └── acc [System]',
+        ],
+    ),
+    (
+        dict(depth=0), [
+            'a',
+        ],
+    ),
+    (
+        dict(depth=1), [
+            'a',
+            '├── aa',
+            '├── ab',
+            '└── ac',
+        ],
+    ),
+    (
+        dict(depth=2, show_class=True), [
+            'a [System]',
+            '└── aa [System]',
+            '    ├── aaa [System]',
+            '    └── aab [System]',
+            '├── ab [System]',
+            '└── ac [System]',
+            '    ├── aca [System]',
+            '    ├── acb [System]',
+            '    └── acc [System]',
+        ],
+    ),
+])
+def test_hierarchy_top(composite, options, expected):
+    content = hierarchy_content(composite, **options)
+    assert content == expected
+
+
+def test_hierarchy_sub(composite):
+    content = hierarchy_content(composite.aa, show_class=False)
+    assert content == [
+        'aa',
+        '├── aaa',
+        '└── aab',
+    ]
+    content = hierarchy_content(composite.ab, show_class=False)
+    assert content == [
+        'ab',
+    ]
+    content = hierarchy_content(composite.ac, show_class=False)
+    assert content == [
+        'ac',
+        '├── aca',
+        '├── acb',
+        '└── acc',
+    ]
