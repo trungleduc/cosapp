3f426c6de530b6c282aee71eca396255a85f9e0e
Etienne Lac <etienne.lac@safrangroup.com>
2025-04-17 10:41:32 +0200

    Expose `EventError` exception in `cosapp.multimode`

--- cosapp/multimode/__init__.py
+++ cosapp/multimode/__init__.py
@@ -1,11 +1,12 @@
 """
 Multimode package of CoSApp.
 """
-from .event import Event, PeriodicTrigger
+from .event import Event, PeriodicTrigger, EventError
 from .discreteStepper import DiscreteStepper
 
 __all__ = [
     "Event",
+    "EventError",
     "PeriodicTrigger",
     "DiscreteStepper",
 ]
